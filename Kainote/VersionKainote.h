07a3920b05dc7be316132bce99c97f398995209c
bjakja <bjakja7@gmail.com>
2022-05-25 23:36:51 +0200

    Build fixes

--- Kainote/VersionKainote.h
+++ Kainote/VersionKainote.h
@@ -20,5 +20,5 @@
 //StableReleaseLink: https://github.com/bjakja/Kainote/releases/download/v0.9.3.893/Kainote.x64.zip
 //VersionKainoteX86: 0.9.3.910
 
-#define VersionKainote "1.0.0.1485"
-#define NumVersionKainote 1,0,0,1485
+#define VersionKainote "1.0.0.1488"
+#define NumVersionKainote 1,0,0,1488
